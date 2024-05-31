06e68edcd2a304dab3fc764f50765ca02afb546e
NivkeKnight <nivkeknight@gmail.com>
2024-05-31 15:43:09 -0300

    Unit 10

---
+++ Arquivos html/script.js
@@ -0,0 +1,11 @@
+var chuva = false;
+var sol = true;
+console.log(chuva);
+console.log(sol);
+var idade = 14;
+var Altura = 1.70;
+console.log(idade)
+console.log(Altura)
+var nome = "Ana";
+var sobrenome = "silva"
+sobrenome = "rocha"
