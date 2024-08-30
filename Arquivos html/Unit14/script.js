3383a5a9672e0cc0d00cacf74cd767f34f79a3c2
NivkeKnight <nivkeknight@gmail.com>
2024-08-30 15:12:36 -0300

    Final aula 30/08

--- Arquivos html/Unit14/script.js
+++ Arquivos html/Unit14/script.js
@@ -1,2 +1,10 @@
 //API Application Programmin Interface
 //Interface de Programação de Aplicações
+$("BotãoBuscarPersonagem").click(buscapersonagemaleatorio);
+ 
+function buscapersonagemaleatorio(){
+    $.get("https://swapi.dev/api/people/1") , preenchercardpersonagem
+}
+function preenchercardpersonagem(data){
+    $("personagemaleatorio").text(data.name)
+}
