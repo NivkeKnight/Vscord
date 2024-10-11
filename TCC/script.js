6cec9433e0fe1abc2ca762ca86b16845da11ca63
NivkeKnight <nivkeknight@gmail.com>
2024-10-11 13:59:37 -0300

    Tcc Parte 3

--- TCC/script.js
+++ TCC/script.js
@@ -1,9 +1,11 @@
 const divResultados = $("#resultados");
 
 $("#pesquisar").click(pesquisarItem);
+$("#search").click(pesquisarArma);
 
 // Função para pesquisar item
-function pesquisarItem(nome) {
+function pesquisarItem() {
+    var nome = $("#ProcuraItem").val()
     $.get("http://localhost:4000/api/items?search=" + nome)
         .done(data => {popularLista(data)
           console.log(data)
@@ -14,9 +16,10 @@ function pesquisarItem(nome) {
 }
 
 // Função para pesquisar arma
-function pesquisarArma(nome) {
-    $.get("http://localhost:4000/api/weapons?search=" + nome)
-        .done(data => popularLista(data))
+function pesquisarArma() {
+    var nome = $("#ProcuraArma").val()
+    $.get("http://localhost:8080/api/weapon/name?value=" + nome)
+        .done(data => popularListaArma(data.Resultado))
         .fail(() => {
             alert("Erro ao buscar armas. Tente novamente.");
         });
@@ -43,6 +46,35 @@ function popularLista(data) {
             <p>${item.InGameDescription}</p>
           </div>
         `;
+
+        divResultados.append(resultado);
+    });
+}
+// Função comum para exibir os resultados de itens ou armas
+function popularListaArma(data) {
+    console.log(data);
+    divResultados.html('');  // Limpa a div de resultados
+
+    if (data.length === 0) {
+        divResultados.append("<p>Nenhum resultado encontrado.</p>");
+        return;
+    }
+
+    data.forEach(item => {
+        const resultado = document.createElement('div');
+        resultado.classList.add('resultado');
+
+        resultado.innerHTML = `
+  <div class="resultado-item">
+    <h3>${item.name}</h3>
+    <p><strong>Ataque Físico:</strong> ${item.atk.physical}</p>
+    <p><strong>Ataque Mágico:</strong> ${item.atk.magic}</p>
+    <p><strong>Ataque de Fogo:</strong> ${item.atk.fire}</p>
+    <p><strong>Ataque de Relâmpago:</strong> ${item.atk.lightning}</p>
+    <p><strong>Bônus de Ataque:</strong> ${item.atk.bonus}</p>
+  </div>
+`;
+
 
         divResultados.append(resultado);
     });
